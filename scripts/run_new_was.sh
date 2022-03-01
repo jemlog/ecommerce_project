29f41f56a42dc3ede9e3d914a1902fbc0bcfd8d2
jemin <jemin03120111@gmail.com>
2022-03-01 22:55:02 +0900

    application.yml 분리

--- scripts/run_new_was.sh
+++ scripts/run_new_was.sh
@@ -20,7 +20,7 @@ fi
 
 echo "hi"
 
-nohup java -jar -Dserver.port=${TARGET_PORT} -Dspring.config.location=classpath:application.yml,/home/ec2-user/application-real-db.yml /home/ec2-user/playground/finalproject/build/libs/* > /home/ec2-user/nohup.out 2>&1 &
+nohup java -jar -Dserver.port=${TARGET_PORT} -Dspring.config.location=classpath:application-real.yml,/home/ec2-user/application-real-db.yml /home/ec2-user/playground/finalproject/build/libs/* > /home/ec2-user/nohup.out 2>&1 &
 
 echo "> Now new WAS runs at ${TARGET_PORT}."
 exit 0
