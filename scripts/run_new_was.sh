98792e56ecaecdd9343f405d7a441c5652a5a45b
jemin <jemin03120111@gmail.com>
2022-03-02 00:28:07 +0900

    profile.active 변경

--- scripts/run_new_was.sh
+++ scripts/run_new_was.sh
@@ -19,7 +19,7 @@ if [ ! -z ${TARGET_PID} ]; then
 fi
 
 echo "hi"
-nohup java -jar -Dserver.port=${TARGET_PORT} -Dspring.config.location=classpath:application.yml,/home/ec2-user/application-real-db.yml -Dspring.profiles.active=real /home/ec2-user/playground/finalproject/build/libs/* > /home/ec2-user/nohup.out 2>&1 &
+nohup java -jar -Dserver.port=${TARGET_PORT} -Dspring.config.location=/home/ec2-user/application-real-db.yml -Dspring.profiles.active=real /home/ec2-user/playground/finalproject/build/libs/* > /home/ec2-user/nohup.out 2>&1 &
 
 echo "> Now new WAS runs at ${TARGET_PORT}."
 exit 0
