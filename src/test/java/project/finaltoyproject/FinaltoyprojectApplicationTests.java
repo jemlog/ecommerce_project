04a030dee971fc1d87c271e967e74f01b2a39eb0
jemin <jemin03120111@gmail.com>
2022-03-05 15:15:27 +0900

    contextLoads 빌드 시 동작 시험

--- src/test/java/project/finaltoyproject/FinaltoyprojectApplicationTests.java
+++ src/test/java/project/finaltoyproject/FinaltoyprojectApplicationTests.java
@@ -3,7 +3,7 @@ package project.finaltoyproject;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class FinaltoyprojectApplicationTests {
 
     @Test
