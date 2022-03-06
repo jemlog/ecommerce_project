72642de000ea16b7f3d623ef42785c242c393353
jemin <jemin03120111@gmail.com>
2022-03-06 17:49:40 +0900

    vuetify 추가

--- vue-frontend/vue.config.js
+++ vue-frontend/vue.config.js
@@ -1,12 +1,18 @@
 module.exports = {
     outputDir: "../src/main/resources/static",
     indexPath: "../static/index.html",
+
     devServer: {
         proxy: "http://localhost:8083"
     },
+
     chainWebpack: config => {
         const svgRule = config.module.rule("svg");
         svgRule.uses.clear();
         svgRule.use("vue-svg-loader").loader("vue-svg-loader");
-    }
+    },
+
+    transpileDependencies: [
+      'vuetify'
+    ]
 };
