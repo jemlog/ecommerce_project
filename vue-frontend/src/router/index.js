c345106bb8a61c4193293b49659e06758071382e
jemin <jemin03120111@gmail.com>
2022-03-06 19:19:25 +0900

    .

--- vue-frontend/src/router/index.js
+++ vue-frontend/src/router/index.js
@@ -7,7 +7,7 @@ const router = new VueRouter({
   routes: [
     {
       path: '/',
-      redirect: '/login',
+      component: () => import('@/views/LoginPage.vue'),
     },
     {
       path: '/login',
