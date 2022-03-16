10e2f1772f18d3a1b55fe42344f5ef75e6a6399e
jemin <jemin03120111@gmail.com>
2022-03-16 23:55:10 +0900

    신용카드 결제 로직 추가중

--- src/main/java/project/finaltoyproject/policy/CreditCardPaymentPolicy.java
+++ src/main/java/project/finaltoyproject/policy/CreditCardPaymentPolicy.java
@@ -2,16 +2,9 @@ package project.finaltoyproject.policy;
 
 public class CreditCardPaymentPolicy implements PaymentPolicy{
 
+
     @Override
-    public int payment(Card creditCard) {
-
-        switch (creditCard)
-        {
-            case KOOKMIN:
-
-            case SHINHAN:
-
-            case
-        }
+    public int payment(String creditCard) {
+        return 0;
     }
 }
