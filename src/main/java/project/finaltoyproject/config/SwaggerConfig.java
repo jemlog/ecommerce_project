b29c0de3dd4f33b099464c9a4fb53bdfc6759d7a
jemin <jemin03120111@gmail.com>
2022-03-20 16:00:47 +0900

    .

---
+++ src/main/java/project/finaltoyproject/config/SwaggerConfig.java
@@ -0,0 +1,10 @@
+package project.finaltoyproject.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+@Be
+}
