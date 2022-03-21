70f786fa9356dd0e6125a2324dcb3fd4233dc77f
jemin <jemin03120111@gmail.com>
2022-03-22 00:36:32 +0900

    querydsl compile 진행

--- src/main/java/project/finaltoyproject/service/ItemService.java
+++ src/main/java/project/finaltoyproject/service/ItemService.java
@@ -9,7 +9,6 @@ import project.finaltoyproject.domain.item.OptionSpecification;
 import project.finaltoyproject.domain.item.dto.ItemRequestDto;
 import project.finaltoyproject.domain.item.repository.ItemRepository;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -24,22 +23,6 @@ public class ItemService {
     @Transactional
     public Item saveItemAndOptions(ItemRequestDto itemRequestDto,String path)
     {
-        // 기본 가격을 표시한 optionSpecification 생성
-        //   List<OptionSpecification> options = new ArrayList<>();
-        //   options.add(optionSpec);
-        // 기본 가격 optionSpecification을 포함한 optionGroupSpecification
-        //     OptionGroupSpecification optionGroupSpec = new OptionGroupSpecification(itemRequestDto.getOptionGroupName(),
-        //    itemRequestDto.isExclusive(),itemRequestDto.isBasic(), options);
-    //    OptionSpecification optionSpec = new OptionSpecification(itemRequestDto., itemRequestDto.getOptionPrice());
-
-    //   OptionGroupSpecification basicOptGroupSpec = OptionGroupSpecification.basic(itemRequestDto.getOptionGroupName(),
-    //            itemRequestDto.isExclusive(), optionSpec);
-
-        // 위의 optinGroupSpecification을 포함한 item 생성
-        // 이제 price는 item의 optinGroupSpecification에서 가져오면 된다.
-    //    Item newItem = Item.builder().itemName(itemRequestDto.getItemName()).s3ImagePath(path)
-    //            .quantity(itemRequestDto.getQuantity())
-    //            .basic(basicOptGroupSpec).build();
         Item item = new Item(itemRequestDto.getItemName(),
                 itemRequestDto.getQuantity(),
                 path,
